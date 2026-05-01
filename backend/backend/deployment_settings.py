27caf66fa8ba78fb4f37a72f8283d11471d8d9a4
anouar <anouar.khaled2002@gmail.com>
2026-05-01 19:58:55 +0200

    change the name of the deployment setting file

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -27,9 +27,3 @@ STORAGES={
     },
     
 }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
--- backend/build.sh
+++ backend/build.sh
@@ -2,4 +2,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
