57e46d13ace071b77586ded8a2637c24a035b68d
anouar <anouar.khaled2002@gmail.com>
2026-05-02 19:27:38 +0200

    fix config

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-if ["$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
+if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
     python manage.py createsuperuser --noinput
 fi
